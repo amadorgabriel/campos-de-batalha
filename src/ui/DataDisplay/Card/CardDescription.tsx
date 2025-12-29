5d16f02b6eb4514906b56e0e39b6a6d6cdef3c67
Gabriel Amador <gabrielramador2014@gmail.com>
2025-12-28 23:44:53 -0300

    feat: add card component

---
+++ src/ui/DataDisplay/Card/CardDescription.tsx
@@ -0,0 +1,11 @@
+import { cn } from '@/shared/utils/cn';
+import React from 'react';
+
+export const CardDescription = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div ref={ref} className={cn('text-sm text-muted-foreground', className)} {...props} />
+));
+
+CardDescription.displayName = 'CardDescription';
---
+++ src/ui/DataDisplay/Card/CardTitle.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { cn } from '@/shared/utils/cn';
+
+export const CardTitle = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...props }, ref) => (
+    <div
+      ref={ref}
+      className={cn('text-2xl font-semibold leading-none tracking-tight', className)}
+      {...props}
+    />
+  )
+);
+
+CardTitle.displayName = 'CardTitle';
